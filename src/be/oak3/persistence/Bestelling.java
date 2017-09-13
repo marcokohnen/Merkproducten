5154c31ea7c8e40937b165a4259cc766911205f1
Marco <m.kohnen@telenet.be>
2017-09-13 13:30:18 +0200

    Interface Berekenbaar en Bestelling afgewerkt

---
+++ src/be/oak3/persistence/Bestelling.java
@@ -0,0 +1,15 @@
+package be.oak3.persistence;
+
+import be.oak3.model.Berekenbaar;
+import be.oak3.model.Product;
+
+public interface Bestelling extends Berekenbaar {
+
+    void voegProductToe(Product product);
+    void sorteer();
+    void sorteerOpMerk();
+    void sorteerOpVolume();
+    void toonPerMerk();
+    void toonGoedkopeProducten();
+    void zoekDuursteProduct();
+}
